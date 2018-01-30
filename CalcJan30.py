f31bb9295a4dda6032e9587f19e576d4b0cf1d11
rubydiarmit <34691001+rubydiarmit@users.noreply.github.com>
2018-01-30 13:36:48 -0500

    Add files via upload

---
+++ CalcJan30.py
@@ -0,0 +1,11 @@
+print("This program calculates miles per gallon.")
+milesDriven = float(input("Miles driven: "))
+gallonsUsed = float(input("Gallons used: "))
+mpg = milesDriven/gallonsUsed
+print("Miles per gallon:", mpg, "miles")
+'''
+I used print in lines 1 and 5 in order to print a statment
+I used float and input in lines 2 and 3 in order to both print a statement and also allow the user to type their own
+In order to define the variables based on the users input, I included the variables at the beginnging with an "="
+
+'''
